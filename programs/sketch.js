88a17e6385ecc0d800f05dfe760dbbb5c7fdb4fd
CyberKing4554 <158240388+CyberKing4554@users.noreply.github.com>
2025-02-23 20:12:25 -0800

    d

--- programs/sketch.js
+++ programs/sketch.js
@@ -19,7 +19,7 @@ function setup(){
 function draw(){
   background(255);
   numberOfBoxes = 0;
-  drawContentBox("Hackpack Turret Over Serial + Extensions","This program allows you to use a wireless serial communicator (such as a HC dash zero five or HC dash zero six) to control your arduino nano. Along with adding compatablilty for certain arduino sensors.",220,0,turretSerialImg,80, 'https://google.com');
+  drawContentBox("Hackpack Turret Over Serial + Extensions","This program allows you to use a wireless serial communicator (such as a HC-05 or HC-06) to control your arduino nano. Along with adding compatablilty for certain arduino sensors.",220,0,turretSerialImg,80, 'https://google.com');
   drawContentBox("             Jumpixel v_1.0             ","A two player game created using Microsoft Makecode Arcade. (please note that you must host a multiplayer game to play).",220,0,jumpixelImg,80, 'https://arcade.makecode.com/S58103-16584-58261-03006');
   //dominoSerialImg.resize(325,172);
   //drawContentBox("Hackpack Domino Over Serial + Extensions","This program allows you to use a wireless serial communicator (such as a HC-05 or HC-06) to control your arduino nano.",220,0,dominoSerialImg,80, 'https://google.com');
