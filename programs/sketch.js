8680c05922e935a00b0c271d16d7e96038598f37
CyberKing4554 <158240388+CyberKing4554@users.noreply.github.com>
2025-02-23 19:50:03 -0800

    d

--- programs/sketch.js
+++ programs/sketch.js
@@ -9,11 +9,11 @@ function setup(){
   createCanvas(windowWidth,1500);
   topBarX = width;
   topBarX -= 140;
-  font2 = loadFont('/assets/font2.ttf');
-  turretSerialImg = loadImage('/assets/Turret.jpg');
-  dominoSerialImg = loadImage("/assets/dom.jpg");
-  jumpixelImg = loadImage('/assets/jumpixel.png');
-  font1 = loadFont('/assets/font1.ttf');
+  font2 = loadFont('assets/font2.ttf');
+  turretSerialImg = loadImage('assets/Turret.jpg');
+  dominoSerialImg = loadImage("assets/dom.jpg");
+  jumpixelImg = loadImage('assets/jumpixel.png');
+  font1 = loadFont('assets/font1.ttf');
 }
 
 function draw(){
